4ef1080976c273de079dcf2d391b5f2b56002e63
MOISES SOUSA SANTOS <46007503+moisesssantos@users.noreply.github.com>
2025-11-09 11:30:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -28,9 +28,9 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
-  },
+  outDir: "dist",
+  emptyOutDir: true,
+},
   server: {
     fs: {
       strict: true,
